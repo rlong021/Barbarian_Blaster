bd79a827390f1336df233d346f2a9db3a2c25e7e
Rogelio Long <rlong021@nmsu.edu>
2026-05-05 15:44:33 -0600

    added raycaster

---
+++ rayPickerCamera/ray_picker_camera.gd
@@ -0,0 +1,14 @@
+extends Camera3D
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) *100.0
+	ray_cast_3d.force_raycast_update()
+	
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			pass
