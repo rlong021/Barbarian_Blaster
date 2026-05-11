29ab5916352468d5f36bfac043e7e200aaa3baf1
Rogelio Long <rlong021@gmail.com>
2026-05-10 22:34:04 -0600

    turrets added

---
+++ scenes/turret_manager.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var turret: PackedScene
+
+func build_turret(turret_position:Vector3) -> void:
+	var new_turret = turret.instantiate()
+	add_child(new_turret)
+	new_turret.global_position = turret_position
