0d4728b70aca64149b4ff4bf5db5f4ef9bb30caf
Rogelio Long <rlong021@gmail.com>
2026-05-17 18:10:57 -0600

    index

--- Index.service.worker.js
+++ Index.service.worker.js
@@ -4,20 +4,20 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779062899|8127883831';
+const CACHE_VERSION = '1779063042|8270950270';
 /** @type {string} */
 const CACHE_PREFIX = 'Barbarian_Blaste-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
-const OFFLINE_URL = 'Index.offline.html';
+const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["Index.html","Index.js","Index.offline.html","Index.icon.png","Index.apple-touch-icon.png","Index.audio.worklet.js","Index.audio.position.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["Index.wasm","Index.pck"];
+const CACHEABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
